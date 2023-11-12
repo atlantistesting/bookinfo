2c40bbd9af85dc3781c72944e545d775814bfb50
Brian Adams <service@contrasting.org>
2023-11-12 17:26:29 -0500

    Hard code vpc subnet for now

--- subnets.tf
+++ subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "private_01" {
-  vpc_id     = data.aws_vpcs.main.id
+  vpc_id     = vpc-0205bec3c9307208c
   cidr_block = "10.0.101.0/24"
 
   tags = {
@@ -7,47 +7,3 @@ resource "aws_subnet" "private_01" {
   }
 }
 
-resource "aws_subnet" "private_02" {
-  vpc_id     = data.aws_vpcs.main.id
-  cidr_block = "10.0.102.0/24"
-
-  tags = {
-    Name = "private_02"
-  }
-}
-
-resource "aws_subnet" "private_03" {
-  vpc_id     = data.aws_vpcs.main.id
-  cidr_block = "10.0.103.0/24"
-
-  tags = {
-    Name = "private_03"
-  }
-}
-
-resource "aws_subnet" "public_01" {
-  vpc_id     = data.aws_vpcs.main.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "public_01"
-  }
-}
-
-resource "aws_subnet" "public_02" {
-  vpc_id     = data.aws_vpcs.main.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "public_02"
-  }
-}
-
-resource "aws_subnet" "public_03" {
-  vpc_id     = data.aws_vpcs.main.id
-  cidr_block = "10.0.3.0/24"
-
-  tags = {
-    Name = "public_03"
-  }
-}
