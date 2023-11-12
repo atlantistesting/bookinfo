e30d4daf135cfcba5e02eb0b843bb54b2a64a615
Brian Adams <service@contrasting.org>
2023-11-12 18:39:14 -0500

    No need to create VPC

--- subnets.tf
+++ subnets.tf
@@ -3,7 +3,42 @@ resource "aws_subnet" "private_01" {
   cidr_block = "10.0.0.0/24"
 
   tags = {
+    Name = "private_00"
+  }
+}
+
+resource "aws_subnet" "private_02" {
+  vpc_id     = "vpc-0a762c08e9eaf51f7"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
     Name = "private_01"
   }
 }
 
+resource "aws_subnet" "private_02" {
+  vpc_id     = "vpc-0a762c08e9eaf51f7"
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private_02"
+  }
+}
+
+resource "aws_subnet" "public_01" {
+  vpc_id     = "vpc-0a762c08e9eaf51f7"
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "public_01"
+  }
+}
+
+resource "aws_subnet" "public_02" {
+  vpc_id     = "vpc-0a762c08e9eaf51f7"
+  cidr_block = "10.0.4.0/24"
+
+  tags = {
+    Name = "private_02"
+  }
+}
