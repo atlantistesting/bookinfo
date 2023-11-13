895715ce7a6fb7f9a5683f76f6102740d5ecf1c5
Brian Adams <service@contrasting.org>
2023-11-13 08:47:53 -0500

    Add requirements

---
+++ requirements.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.45"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.18.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
+  }
+}
