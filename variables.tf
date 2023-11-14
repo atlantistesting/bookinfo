1503d771316428f23013c101aaaaacf4f98d838b
Brian Adams <service@contrasting.org>
2023-11-14 11:00:47 -0500

    Use default region for now

--- variables.tf
+++ variables.tf
@@ -1,37 +1,7 @@
 variable "region" {
   description = "The region where to provision resources"
   type        = string
-}
-
-variable "zerossl_eab_key_id" {
-  description = "ZeroSSL eab key id"
-  type        = string
-}
-
-variable "zerossl_eab_hmac_key" {
-  description = "ZeroSSL eab hmac key"
-  type        = string
-}
-
-variable "zerossl_email" {
-  description = "ZeroSSL email"
-  type        = string
-}
-
-variable "argocd_oidc_client_id" {
-  description = "ArgoCD OIDC Client ID"
-  type        = string
-}
-
-variable "argocd_oidc_client_secret" {
-  description = "ArgoCD OIDC Client Secret"
-  type        = string
-}
-
-variable "domain_name" {
-  description = "Primary domain name"
-  type        = list(any)
-  default     = ["wayofthesys.org"]
+  default     = "ap-southeast-1"
 }
 
 variable "github_pat" {
