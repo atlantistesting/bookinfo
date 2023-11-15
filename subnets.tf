e5f0b09674d73204d958145a61ea3fbf8057a0d9
Brian Adams <service@contrasting.org>
2023-11-14 20:28:12 -0500

    Hard code subnets

--- subnets.tf
+++ subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "private_00" {
-  vpc_id     = vpc-0eb767eb3b5a0342f
+  vpc_id     = "vpc-0eb767eb3b5a0342f"
   cidr_block = "10.0.0.0/24"
 
   tags = {
@@ -8,7 +8,7 @@ resource "aws_subnet" "private_00" {
 }
 
 resource "aws_subnet" "private_01" {
-  vpc_id     = vpc-0eb767eb3b5a0342f
+  vpc_id     = "vpc-0eb767eb3b5a0342f"
   cidr_block = "10.0.1.0/24"
 
   tags = {
@@ -17,7 +17,7 @@ resource "aws_subnet" "private_01" {
 }
 
 resource "aws_subnet" "private_02" {
-  vpc_id     = vpc-0eb767eb3b5a0342f
+  vpc_id     = "vpc-0eb767eb3b5a0342f"
   cidr_block = "10.0.2.0/24"
 
   tags = {
@@ -26,7 +26,7 @@ resource "aws_subnet" "private_02" {
 }
 
 resource "aws_subnet" "public_00" {
-  vpc_id     = vpc-0eb767eb3b5a0342f
+  vpc_id     = "vpc-0eb767eb3b5a0342f"
   cidr_block = "10.0.3.0/24"
 
   tags = {
@@ -35,7 +35,7 @@ resource "aws_subnet" "public_00" {
 }
 
 resource "aws_subnet" "public_01" {
-  vpc_id     = vpc-0eb767eb3b5a0342f
+  vpc_id     = "vpc-0eb767eb3b5a0342f"
   cidr_block = "10.0.4.0/24"
 
   tags = {
@@ -44,7 +44,7 @@ resource "aws_subnet" "public_01" {
 }
 
 resource "aws_subnet" "public_02" {
-  vpc_id     = vpc-0eb767eb3b5a0342f
+  vpc_id     = "vpc-0eb767eb3b5a0342f"
   cidr_block = "10.0.5.0/24"
 
   tags = {
