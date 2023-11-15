3bcaddbf4da455b353cb2d3a07888527c679d6b2
Brian Adams <service@contrasting.org>
2023-11-15 17:56:37 -0500

    Move eks to bac temporarily

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private_00" {
   vpc_id     = module.vpc.vpc_id
-  cidr_block = module.vpc.private_subnets
+  cidr_block = module.vpc.public_subnets
 
   tags = {
     Name = "private_00"
@@ -9,7 +9,7 @@ resource "aws_subnet" "private_00" {
 
 resource "aws_subnet" "private_01" {
   vpc_id     = module.vpc.vpc_id
-  cidr_block = module.vpc.private_subnets
+  cidr_block = module.vpc.public_subnets
 
   tags = {
     Name = "private_01"
@@ -18,7 +18,7 @@ resource "aws_subnet" "private_01" {
 
 resource "aws_subnet" "private_02" {
   vpc_id     = module.vpc.vpc_id
-  cidr_block = module.vpc.private_subnets
+  cidr_block = module.vpc.public_subnets
 
   tags = {
     Name = "private_02"
@@ -27,7 +27,7 @@ resource "aws_subnet" "private_02" {
 
 resource "aws_subnet" "public_00" {
   vpc_id     = module.vpc.vpc_id
-  cidr_block = module.vpc.private_subnets
+  cidr_block = module.vpc.public_subnets
 
   tags = {
     Name = "public_00"
@@ -36,7 +36,7 @@ resource "aws_subnet" "public_00" {
 
 resource "aws_subnet" "public_01" {
   vpc_id     = module.vpc.vpc_id
-  cidr_block = module.vpc.private_subnets
+  cidr_block = module.vpc.public_subnets
 
   tags = {
     Name = "public_01"
@@ -45,7 +45,7 @@ resource "aws_subnet" "public_01" {
 
 resource "aws_subnet" "public_02" {
   vpc_id     = module.vpc.vpc_id
-  cidr_block = module.vpc.private_subnets
+  cidr_block = module.vpc.public_subnets
 
   tags = {
     Name = "public_02"
