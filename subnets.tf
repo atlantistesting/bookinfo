aad017e6ea7e839eaee553a77a8a6942f88b6bfa
Brian Adams <service@contrasting.org>
2023-11-15 18:00:00 -0500

    Changed subnets from private to public

--- subnets.tf
+++ subnets.tf
@@ -1,30 +1,3 @@
-resource "aws_subnet" "private_00" {
-  vpc_id     = module.vpc.vpc_id
-  cidr_block = module.vpc.public_subnets
-
-  tags = {
-    Name = "private_00"
-  }
-}
-
-resource "aws_subnet" "private_01" {
-  vpc_id     = module.vpc.vpc_id
-  cidr_block = module.vpc.public_subnets
-
-  tags = {
-    Name = "private_01"
-  }
-}
-
-resource "aws_subnet" "private_02" {
-  vpc_id     = module.vpc.vpc_id
-  cidr_block = module.vpc.public_subnets
-
-  tags = {
-    Name = "private_02"
-  }
-}
-
 resource "aws_subnet" "public_00" {
   vpc_id     = module.vpc.vpc_id
   cidr_block = module.vpc.public_subnets
