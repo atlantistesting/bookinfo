40b1b57dad770802a45cd4aade73d14935c71c07
Brian Adams <service@contrasting.org>
2023-11-17 16:45:39 -0500

    Declare aws vpc main

--- subnets.tf
+++ subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "public_01" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = module.vpc.vpc_id
   cidr_block = "10.0.1.0/24"
 
   tags = {
@@ -9,7 +9,7 @@ resource "aws_subnet" "public_01" {
 
 
 resource "aws_subnet" "public_02" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = module.vpc.vpc_id
   cidr_block = "10.0.2.0/24"
 
   tags = {
@@ -18,7 +18,7 @@ resource "aws_subnet" "public_02" {
 }
 
 resource "aws_subnet" "public_03" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = module.vpc.vpc_id
   cidr_block = "10.0.3.0/24"
 
   tags = {
